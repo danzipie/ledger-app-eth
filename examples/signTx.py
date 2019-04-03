dfaf41a1d29a1629b67bf1fc6aa4c4c68a7b6f9c
danzipie <danzipie@gmail.com>
2019-04-03 17:20:15 +0200

    makes CHAIN arg input

--- examples/signTx.py
+++ examples/signTx.py
@@ -58,7 +58,7 @@ parser.add_argument('--startgas', help="startgas", default='21000', type=int)
 parser.add_argument('--amount', help="Amount to send in ether", required=True)
 parser.add_argument('--to', help="Destination address", type=str, required=True)
 parser.add_argument('--path', help="BIP 32 path to sign with")
-parser.add_argument('--data', help="Data to add, hex encoded")
+parser.add_argument('--data', help="Data to add, int encoded")
 args = parser.parse_args()
 
 if args.path == None:
